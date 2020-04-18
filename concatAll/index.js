046f0f30f1460753205a94dfe9fefdc83aabc60a
bms241093 <balmukund.singh241@gmail.com>
2020-04-18 21:10:54 +0530

    Study of concatAll() operator with comments completed

--- concatAll/index.js
+++ concatAll/index.js
@@ -1,10 +1,16 @@
 import Rx from 'rxjs';
 import { fromStdIn } from '../utility';
+/*
+  Note: concatAll()
+
+*/
 
 Rx.Observable.of(
-    Rx.Observable.range(1,3),
-    Rx.Observable.interval(100).take(10),
-    fromStdIn()
+    Rx.Observable.range(1,3),              // First Observable will get executed then
+    Rx.Observable.interval(100).take(10),  // Second Observable will get executed then
+    fromStdIn()                            // Third Observable
 )
-    .concatAll()
+//   concatAll()  operator sequentially run one by one Observable , even though u try to execute 
+//   third Observable from starting , it won't work.
+    .concatAll()                          
     .subscribe(a=>console.log(a));
