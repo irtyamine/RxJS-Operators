18db7bc52d70c3f2601a42ea8cdf60d6332f4d9f
bms241093 <balmukund.singh241@gmail.com>
2020-04-18 19:33:13 +0530

    Study of last() operator with comments completed

--- last/index.js
+++ last/index.js
@@ -1,8 +1,12 @@
 import Rx from 'rxjs';
+/* Note:
+   1. last()               => It will give last element.
+   2. last(n=> n % 2 == 0) => Whoever is the last element who will satisfy this condition.  
+*/
 
 Rx.Observable
     // .range(1,9)
     .interval(100)
     .take(10)
-    .last(n=> n % 2 == 0)
+    .last(n=> n % 2 == 0)        
     .subscribe(a=>console.log(a));
