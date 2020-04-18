09e618bd158b3b1f1d9e4da7407babbe616aa8f2
bms241093 <balmukund.singh241@gmail.com>
2020-04-18 15:26:25 +0530

    filter() operator has used range() operator

--- filter/index.js
+++ filter/index.js
@@ -1,13 +1,15 @@
 import Rx from 'rxjs';
-/* Note: 3 Way to give example of filter
+/* Note: 4 Way to give example of filter
    1. interval(duration) == it will same as array[1,2,3,4,5,6,7,8,9,10]
-   2. We can use array and then use from() operator to use filter
-   3. timer(duration , interval) 
+   2. timer(duration , interval) 
+   3. We can use Array and then use from() operator to use filter
+   4. range(start , end) operator can be used also
+   
 
      ********kindly comment any one Approach to see output***********************
  */ 
 
-// Approach 1
+// Approach 1 Using interval()
 const interval = Rx.Observable.interval(200);
  
 interval
@@ -15,7 +17,7 @@ interval
     .take(10)
     .subscribe(a=>console.log('Divisible by 10 ==', a));
 // ----------------------------------------------------------------------------------
-// Approach 2
+// Approach 2 Using timer()
 const timer = Rx.Observable.timer(200,100);
  
 timer
@@ -31,3 +33,10 @@ const arrayObservables = Rx.Observable.from(array);
 arrayObservables
      .filter(n=>n % 2 == 0)
      .subscribe(a=>console.log('Divisible by 2 ==',a));
+
+// ----------------------------------------------------------------------------------
+ // Approach 4 : Using range() operator
+ const rangeObservable = Rx.Observable.range(0,16);
+
+ rangeObservable
+    .subscribe(e=>console.log(`Range emitted ${e}`));   
