098deab3b15fa1e3938f6cc57ed023037157f495
bms241093 <balmukund.singh241@gmail.com>
2020-04-18 13:52:11 +0530

    Study of every() operator with comments completed

--- every/index.js
+++ every/index.js
@@ -1,5 +1,9 @@
 import Rx from 'rxjs';
-
+/* Note:
+   1. every() operator always emits its value in last
+   2. If every() operator boolean condition gets false , it stop immediately.
+   3. But if condition remains true then it will emit value at the end.
+*/
 const range = Rx
     .Observable
     .interval(100)
@@ -8,5 +12,6 @@ const range = Rx
 range
     .map(n=>n*2)
     .do(v=>console.log("Value::",v))
+    .take(10)
     .every(n=>n % 2 == 0)
     .subscribe(b=>console.log(b));
