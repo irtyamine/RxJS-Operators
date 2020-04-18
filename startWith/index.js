2e161a74389c53ff843a64a96d23b6e0c74e6418
bms241093 <balmukund.singh241@gmail.com>
2020-04-18 13:20:46 +0530

    Study of startWith() operator with comments completed

--- startWith/index.js
+++ startWith/index.js
@@ -1,8 +1,10 @@
 import Rx from 'rxjs';
-
+/* Note:
+   1. startWith() operator is used to get desired value first right away then it continues with the normal emitting values.
+*/
 console.info("MADE API REQUEST");
 Rx.Observable
-    .timer(4000,500)
+    .timer(2000,400)
     .map(n=>n+1)
-    .startWith(0)
+    .startWith(10)
     .subscribe(a=>console.log("REQUEST COMPLETE:", a));
