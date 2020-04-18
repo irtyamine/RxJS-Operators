8f223d062808b210ddc440ca0396033f6b41adff
bms241093 <balmukund.singh241@gmail.com>
2020-04-18 17:01:35 +0530

    Study of take() , takeWhile() and takeUntil() operators with comments completed

--- take/index.js
+++ take/index.js
@@ -1,8 +1,14 @@
 import Rx from 'rxjs';
+/*
+  Note:
+  1. takeUntil(Rx.Observable.timer(2000)) operator accepts Observables argument in the form of time
+  2. takeWhile() operator will keep showing data until condition gets false.
+  3. take(count) operator will show limited data.
+*/
 
 Rx.Observable
     //.range(1,9)
-    .interval(100)
+    .interval(500)
     .takeUntil(Rx.Observable.timer(2000))
     // .takeWhile(n=> n < 7)
     // .take(5)
