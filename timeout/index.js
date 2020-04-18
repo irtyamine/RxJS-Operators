67364213b9863de53b0002056a998294a8699511
bms241093 <balmukund.singh241@gmail.com>
2020-04-18 22:49:23 +0530

    Study of timeout() operator with comments completed

--- timeout/index.js
+++ timeout/index.js
@@ -1,9 +1,14 @@
 import Rx from 'rxjs';
 import { fromStdIn } from '../utility'
+/*
+ Note: Interview question => Diff between timer() and timeout()
+ 1. timer() => Never throw error
+ 2. timeout() => It always throw error after timeout
+*/
 
 console.log("Please INPUT your password QUICKLY!!");
 
 fromStdIn()
     .take(10)
-    .timeout(2000)
+    .timeout(2000)   // It throws error
     .subscribe(a=>console.log(a));
