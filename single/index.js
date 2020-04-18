6013dd0068248e8fcbf381004f024da930fbea4f
bms241093 <balmukund.singh241@gmail.com>
2020-04-18 22:00:58 +0530

    Study of single() operator with comments completed

--- single/index.js
+++ single/index.js
@@ -1,5 +1,9 @@
 import Rx from 'rxjs';
-
+/* 
+   Note: Interview Question => Diff between first() and single()
+   first() => Please refer its code , it does not throw error
+   single() => If there is more than one element it throws error
+*/
 Rx.Observable
     .range(0,3)
     .single(n=>n % 2 == 0)
