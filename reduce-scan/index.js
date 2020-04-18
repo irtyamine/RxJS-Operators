864ea3782fa31bbdf51c06d3a41d147b8e266685
bms241093 <balmukund.singh241@gmail.com>
2020-04-18 22:31:11 +0530

    Study of reduce() and scan() operator with comments completed

--- reduce-scan/index.js
+++ reduce-scan/index.js
@@ -1,8 +1,11 @@
 import Rx from 'rxjs';
-
+/*
+  Note: Interview Question
+  How will u add a given array number using RxJS operator => use reduce() or scan()
+*/
 Rx.Observable
     .interval(200)
     .take(10)
-    // .reduce((acc,value)=>acc+value,0)
-    .scan((acc,value)=>acc+value,0)
+    // .reduce((acc,value)=>acc+value,0) // It will return and show total value
+    .scan((acc,value)=>acc+value,0)      // It will keep showing one by one added value output till total value
     .subscribe(a=>console.log(a));
