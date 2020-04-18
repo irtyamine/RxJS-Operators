35842af7ce981990d2599b812d480a702c5e3324
bms241093 <balmukund.singh241@gmail.com>
2020-04-18 14:02:27 +0530

    Study of distinctUntilChanged() operator with comments completed

--- distinctUntilChange/index.js
+++ distinctUntilChange/index.js
@@ -1,5 +1,7 @@
 import Rx from 'rxjs'
-
+/* Note: 
+   1. distinctUntilChanged() operator , is very useful to fetch distinct values from the stream of emit of repeated values.
+*/
 Rx.Observable
     .interval(100)
     .map(n=>~~(n/10))
