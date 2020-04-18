f3a17f63e046892435fb70b09cedd598dd2cb14f
bms241093 <balmukund.singh241@gmail.com>
2020-04-18 12:56:21 +0530

    Study of pluck() operator with comments completed

--- pluck/index.js
+++ pluck/index.js
@@ -1,7 +1,10 @@
 import Rx from 'rxjs'
+/* Note:
+   1. From Object, if you want to get specific value then pass its key to pluck() operator.
 
+*/
 const friendsAPI = Rx.Observable
-    .interval(1000)
+    .interval(300)
     .map(i=>({name:`Friend #${i}`, mutualFriends: i * 2 + 1}))
-    .pluck("mutualFriends")
+    .pluck("name")
     .subscribe(a=>console.log(a));
