46fbc55b32ab4156edd8270cf5484f407f3753e1
bms241093 <balmukund.singh241@gmail.com>
2020-04-19 11:55:10 +0530

    Study of publish(), share() and multicast() operator with comments completed

--- multicast/index.js
+++ multicast/index.js
@@ -1,11 +1,14 @@
 import Rx from 'rxjs';
-
+/* Interview Question : 
+   Your Observables logic should be run one time only , different subscriber should get same result
+   Ans : Use publish() => it will share same result with different subscriber.
+*/
 const shared = new Rx.BehaviorSubject(-1);
 const currencyTicker = Rx.Observable
     .interval(1000)
     .do(a=>console.log("PINGED API"))
     .map(n=>`Currency Info #${n} - [placeholder]`)
-    // .share()
+    // .share()  // When u r using share() , comment currencyTicker.connect(); line
     // .publish()
     .multicast(shared)
 
